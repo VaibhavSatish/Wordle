43df756814f5cb4882858292f522d1acf6d61b79
Vaibhav Satish <vaibhavsatish21@hotmail.com>
2026-06-29 22:42:12 -0700

    started work on main.py (backend in progress)

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from words import VALID_WORDS
 
-app = FastAPI()
+app = FastAPI("Custom Wordle API")
 @app.get("/")
 def root():
     return {"message": "Wordle Backend is Alive"}
