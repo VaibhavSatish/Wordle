c16e2fbf60d4d543e50e06d0cad062e024474d2e
Vaibhav Satish <vaibhavsatish21@hotmail.com>
2026-07-24 11:47:10 -0700

    changed the nltk.corpus to wordnet

--- Backend/words.py
+++ Backend/words.py
@@ -1,9 +1,9 @@
 import nltk
-nltk.download('words')
-from nltk.corpus import words
+nltk.download('wordnet')
+from nltk.corpus import wordnet
 
 def generate_words(word_length) -> list[str]:
     'Generates a list of words of the specified length from the English words set.'
-    words_dict = list(w.lower() for w in words.words())
+    words_dict = list(w.lower() for w in wordnet.words())
     words_of_length = [word for word in words_dict if len(word) == word_length]
     return words_of_length
